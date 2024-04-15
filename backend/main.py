d8cbb85670d0323b3d8fb87d98d662c9cea7c84c
KacperST <kstankiewicz@student.agh.edu.pl>
2024-04-15 22:47:28 +0200

    Add newlines

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,14 @@ from database import SessionLocal
 
 app = FastAPI()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
 
 @app.get("/")
 def read_root(db: Session = Depends(get_db)):
