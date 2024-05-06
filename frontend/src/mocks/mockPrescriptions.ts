716eea8b4a87fa9dd0316a2121d2c4dda7196770
dubielel <mdubielel@gmail.com>
2024-05-06 21:25:54 +0200

    Add mock data

---
+++ frontend/src/mocks/mockPrescriptions.ts
@@ -0,0 +1,24 @@
+import { Prescription } from "@/src/models/prescription";
+
+export const mockPrescriptions: Prescription[] = [
+  { id: 1, doctorId: 101, patientId: 201, description: "Prescription 1" },
+  { id: 2, doctorId: 102, patientId: 202, description: "Prescription 2" },
+  { id: 3, doctorId: 103, patientId: 203, description: "Prescription 3" },
+  { id: 4, doctorId: 104, patientId: 204, description: "Prescription 4" },
+  { id: 5, doctorId: 105, patientId: 205, description: "Prescription 5" },
+  { id: 6, doctorId: 101, patientId: 206, description: "Prescription 6" },
+  { id: 7, doctorId: 102, patientId: 207, description: "Prescription 7" },
+  { id: 8, doctorId: 103, patientId: 201, description: "Prescription 8" },
+  { id: 9, doctorId: 104, patientId: 202, description: "Prescription 9" },
+  { id: 10, doctorId: 105, patientId: 203, description: "Prescription 10" },
+  { id: 11, doctorId: 101, patientId: 204, description: "Prescription 11" },
+  { id: 12, doctorId: 102, patientId: 205, description: "Prescription 12" },
+  { id: 13, doctorId: 103, patientId: 206, description: "Prescription 13" },
+  { id: 14, doctorId: 104, patientId: 207, description: "Prescription 14" },
+  { id: 15, doctorId: 105, patientId: 201, description: "Prescription 15" },
+  { id: 16, doctorId: 101, patientId: 202, description: "Prescription 16" },
+  { id: 17, doctorId: 102, patientId: 203, description: "Prescription 17" },
+  { id: 18, doctorId: 103, patientId: 204, description: "Prescription 18" },
+  { id: 19, doctorId: 104, patientId: 205, description: "Prescription 19" },
+  { id: 20, doctorId: 105, patientId: 206, description: "Prescription 20" },
+];
