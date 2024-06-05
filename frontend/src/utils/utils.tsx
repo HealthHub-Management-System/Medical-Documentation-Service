f1a89a416f5fab3d080960500e6de31cd9ef8ad5
dubielel <mdubielel@gmail.com>
2024-06-06 01:03:00 +0200

    Add fetching users list

--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -24,6 +24,55 @@ export const getCurrentUser = async (sessionCookie: RequestCookie) => {
   } satisfies User;
 };
 
+export type ListAllUsersResponse = {
+  users: User[];
+  total: number;
+  pages: number;
+  currentPage: number;
+};
+
+export const listAllUsers = async (): Promise<User[]> => {
+  const res = await fetch(`http://localhost:8080/api/v1/users`, {
+    method: "GET",
+    cache: "no-store",
+    headers: new Headers({
+      Authorization: "Bearer a9655739cbfed565b6b86dfb4d2a63df",
+    }),
+  });
+
+  if (res.ok) return ((await res.json()) as ListAllUsersResponse).users;
+  else console.error("Failed to fetch users list:", res.statusText);
+
+  return [
+    {
+      id: "a5f96fd2-a29f-4d95-bdcb-1545b59310fd",
+      name: "Mock user",
+      email: "mockuser@mail.com",
+      role: "patient",
+    },
+  ];
+};
+
+export const listPatients = async (): Promise<User[]> => {
+  const allUsers = await listAllUsers();
+
+  return allUsers.reduce((acc: User[], user) => {
+    if (user.role === "patient") acc.push(user);
+
+    return acc;
+  }, []);
+};
+
+export const listDoctors = async (): Promise<User[]> => {
+  const allUsers = await listAllUsers();
+
+  return allUsers.reduce((acc: User[], user) => {
+    if (user.role === "doctor") acc.push(user);
+
+    return acc;
+  }, []);
+};
+
 // TODO correct this function to be more type-safe
 export const snakeToCamel = (obj: any): any => {
   if (obj === null || typeof obj !== "object") {
