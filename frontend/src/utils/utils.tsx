91ef7ef38d1325f443af53a0206f840cce1946e3
dubielel <mdubielel@gmail.com>
2024-06-06 02:03:16 +0200

    Add getCurrentUser on client side

--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -24,6 +24,28 @@ export const getCurrentUser = async (sessionCookie: RequestCookie) => {
   } satisfies User;
 };
 
+export const getCurrentUserClient = async () => {
+  const res = await fetch(`http://localhost:8080/api/v1/users/current`, {
+    method: "GET",
+    cache: "no-store",
+    headers: new Headers({
+      Authorization: "Bearer a9655739cbfed565b6b86dfb4d2a63df",
+    }),
+    credentials: "include",
+  });
+
+  if (res.ok) return (await res.json()) as User;
+  //@ts-ignore
+  else console.error("Failed to fetch user:", res.error ?? res.statusText);
+
+  return {
+    id: "a5f96fd2-a29f-4d95-bdcb-1545b59310fd",
+    name: "Mock user",
+    email: "mockuser@mail.com",
+    role: "patient",
+  } satisfies User;
+};
+
 export type ListAllUsersResponse = {
   users: User[];
   total: number;
