4f00be7ee2ede89c1e32d5a296778b48dc57fcf9
dubielel <mdubielel@gmail.com>
2024-06-06 23:40:47 +0200

    Do medical documentation for doctors

--- frontend/src/app/medical-documentation/page.tsx
+++ frontend/src/app/medical-documentation/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { mockMedicalDocumentations } from "@/src/mocks/mockMedicalDocumentations";
-import { Container, Typography } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { MedicalDocumentationEntryCard } from "./MedicalDocumentationEntryCard";
-import { getCurrentUserClient, snakeToCamel } from "@/src/utils/utils";
+import {
+  getCurrentUserClient,
+  listPatients,
+  snakeToCamel,
+} from "@/src/utils/utils";
 import { MedicalDocumentation } from "@/src/models/medicalDocumentation";
 import { LinkButton } from "../components/LinkButton";
 import { User } from "@/src/models/user";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 const getMedicalDocumentation = async (patientId: string) => {
   const res = await fetch(
@@ -45,16 +59,135 @@ export default function Page() {
     getCurrentUserClient().then((u) => setCurrentUser(u));
   }, []);
 
+  const [patients, setPatients] = useState<User[]>([]);
+  const [filteredPatients, setFilteredPatients] = useState<User[]>(patients);
+  const [selectedPatient, setSelectedPatient] = useState<User | null>(null);
+  useEffect(() => {
+    listPatients().then((p) => {
+      setPatients(p);
+      setFilteredPatients(p);
+    });
+  }, []);
+
   const [medicalDocumentation, setMedicalDocumentation] =
     useState<MedicalDocumentation | null>(null);
   useEffect(() => {
-    if (!currentUser) return;
+    if (!currentUser || currentUser.role === "doctor") return;
     getMedicalDocumentation(currentUser.id).then((r) =>
       setMedicalDocumentation(r)
     );
   }, [currentUser]);
 
+  const { handleSubmit, control, setError } = useForm<{
+    patientName: string;
+  }>();
+
   if (!currentUser) return null;
+
+  if (currentUser.role === "doctor") {
+    return (
+      <Container
+        maxWidth="xl"
+        sx={{
+          m: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Card>
+          <form
+            onSubmit={handleSubmit(async ({ patientName }) => {
+              const patient = patients.find(
+                (patient) => patient.name === patientName
+              );
+              if (!patient) return;
+
+              const patientMedicalDocumentation = await getMedicalDocumentation(
+                patient.id
+              );
+              setSelectedPatient(patient);
+              setMedicalDocumentation(patientMedicalDocumentation);
+            })}
+          >
+            <CardContent
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Controller
+                name="patientName"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Autocomplete
+                    options={filteredPatients.map((patient) => patient.name)}
+                    sx={{ m: 1, width: 300 }}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label="Patient"
+                        error={!!fieldState.error}
+                        helperText={
+                          fieldState.error ? fieldState.error.message : ""
+                        }
+                      />
+                    )}
+                    noOptionsText=""
+                    PaperComponent={({ children }) =>
+                      filteredPatients.length === 0 ? null : (
+                        <Paper>{children}</Paper>
+                      )
+                    }
+                    onInputChange={(_, value) => {
+                      field.onChange(value);
+                      const newFilteredPatients = patients.filter((patient) =>
+                        patient.name.startsWith(value)
+                      );
+                      setFilteredPatients(newFilteredPatients);
+
+                      if (newFilteredPatients.length === 0) {
+                        setError("patientName", {
+                          type: "manual",
+                          message: "Patient not found",
+                        });
+                      }
+                    }}
+                  />
+                )}
+              />
+              <Button variant="contained" type="submit" sx={{ m: 1 }}>
+                Select patient
+              </Button>
+            </CardContent>
+          </form>
+        </Card>
+        {medicalDocumentation && selectedPatient && (
+          <>
+            <Typography variant="h4">
+              Medical documentation for user {selectedPatient.name}
+            </Typography>
+
+            {medicalDocumentation.medicalDocumentationEntries.map((entry) => {
+              return (
+                <MedicalDocumentationEntryCard key={entry.id} entry={entry} />
+              );
+            })}
+            <LinkButton
+              linkProps={{
+                href: `/medical-documentation/${medicalDocumentation.id}/entry/new`,
+              }}
+            >
+              Add new medical documentation entry
+            </LinkButton>
+          </>
+        )}
+      </Container>
+    );
+  }
 
   if (!medicalDocumentation)
     return (
@@ -91,14 +224,6 @@ export default function Page() {
       {medicalDocumentation.medicalDocumentationEntries.map((entry) => {
         return <MedicalDocumentationEntryCard key={entry.id} entry={entry} />;
       })}
-
-      <LinkButton
-        linkProps={{
-          href: `/medical-documentation/${medicalDocumentation.id}/entry/new`,
-        }}
-      >
-        Add new medical documentation entry
-      </LinkButton>
     </Container>
   );
 }
